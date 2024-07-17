a8817ca0c4c4c088f024ded9a9addbdd99e0a306
Jararered <Jararered@icloud.com>
2024-07-17 06:24:20 -0400

    Initial implementation

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -2,26 +2,92 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+const languageCodes = new Map<string, string>([
+    [ "Arabic", "ar" ],     [ "Chinese", "zh" ],    [ "Chinese (Traditional)", "zt" ],
+    [ "Czech", "cs" ],      [ "Danish", "da" ],     [ "Dutch", "nl" ],
+    [ "English", "en" ],    [ "Finnish", "fi" ],    [ "French", "fr" ],
+    [ "German", "de" ],     [ "Greek", "el" ],      [ "Hebrew", "he" ],
+    [ "Hindi", "hi" ],      [ "Hungarian", "hu" ],  [ "Indonesian", "id" ],
+    [ "Irish", "ga" ],      [ "Italian", "it" ],    [ "Japanese", "ja" ],
+    [ "Korean", "ko" ],     [ "Persian", "fa" ],    [ "Polish", "pl" ],
+    [ "Portuguese", "pt" ], [ "Russian", "ru" ],    [ "Slovak", "sk" ],
+    [ "Spanish", "es" ],    [ "Swedish", "sv" ],    [ "Turkish", "tr" ],
+    [ "Ukranian", "uk" ],   [ "Vietnamese", "vi" ],
+]);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "libretranslate-integration" is now active in the web extension host!');
+async function translate(text: string, to: string): Promise<string>
+{
+    const res = await fetch("http://127.0.0.1:5000/translate", {
+        method : "POST",
+        body : JSON.stringify({q : text, source : "auto", target : to, format : "text", alternatives : 0, api_key : ""}),
+        headers : {"Content-Type" : "application/json"}
+    });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('libretranslate-integration.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+    type LibreTranslateResponse = {alternatives : string[], detectedLanguage : string, translatedText : string};
+    const response = await res.json() as LibreTranslateResponse;
+    console.log(response);
+    return response.translatedText;
+}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from LibreTranslate Integration in a web extension host!');
-	});
+export function activate(context: vscode.ExtensionContext)
+{
+    const replaceWithTranslation = vscode.commands.registerCommand('libretranslate-integration.replaceWithTranslation', () => {
+        const editor = vscode.window.activeTextEditor;
+        const selection = editor?.selection;
 
-	context.subscriptions.push(disposable);
+        if (selection && !selection.isEmpty)
+        {
+            const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
+            const highlighted = editor.document.getText(selectionRange);
+
+            vscode.window
+                .showQuickPick(
+                    Array.from(languageCodes.keys()),
+                    )
+                .then(languageChoice => {
+                    if (languageChoice)
+                    {
+                        const code = languageCodes.get(languageChoice);
+                        if (code)
+                        {
+                            translate(highlighted, code).then(data => {editor.edit(editBuilder => { editBuilder.replace(selectionRange, data); })});
+                        }
+                    }
+                });
+        }
+    });
+
+    const insertTranslationBelow = vscode.commands.registerCommand('libretranslate-integration.insertTranslationBelow', () => {
+        const editor = vscode.window.activeTextEditor;
+        const selection = editor?.selection;
+
+        if (selection && !selection.isEmpty)
+        {
+            const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
+            const highlighted = editor.document.getText(selectionRange);
+
+            vscode.window
+                .showQuickPick(
+                    Array.from(languageCodes.keys()),
+                    )
+                .then(languageChoice => {
+                    if (languageChoice)
+                    {
+                        const code = languageCodes.get(languageChoice);
+                        if (code)
+                        {
+                            translate(highlighted, code).then(data => {editor.edit(editBuilder => { editBuilder.insert(selection.end, "\n" + data); })});
+                        }
+                    }
+                });
+        }
+    });
+
+    context.subscriptions.push(replaceWithTranslation);
+    context.subscriptions.push(insertTranslationBelow);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate()
+{
+}
